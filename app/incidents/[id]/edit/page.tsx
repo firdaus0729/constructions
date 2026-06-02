6c38d4f779400acd7679a0049db22625a3bb9770
koko <koko@gmail.com>
2026-06-02 23:08:24 +0900

    fixed

--- app/incidents/[id]/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const EditIncident = dynamic(() => import("./client"), { ssr: false, loading: () => <div>Loading...</div> })
-
-export default EditIncident
--- app/observations/[id]/edit/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const EditObservation = dynamic(() => import("./client"), { ssr: false, loading: () => <div>Loading...</div> })
-
-export default EditObservation
